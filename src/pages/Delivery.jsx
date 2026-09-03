d311ce3999f0f7e3518aa9ecbdc7f156b8512bb3
zeralook <za10az10plaqxm3@gmail.com>
2026-09-03 17:07:11 +0300

    Update Delivery.jsx

--- src/pages/Delivery.jsx
+++ src/pages/Delivery.jsx
@@ -12,8 +12,8 @@ export default function Delivery() {
           <table className="delivery-table">
             <thead><tr><th>المنطقة</th><th>أجور التوصيل</th><th>المدة التقريبية</th></tr></thead>
             <tbody>
-              <tr><td>داخل كربلاء</td><td>3,000 د.ع</td><td>24 ساعة</td></tr>
-              <tr><td>بقية محافظات العراق</td><td>5,000 د.ع</td><td>24 - 48 ساعة</td></tr>
+              <tr><td>داخل كربلاء</td><td>3,000 د.ع</td><td>12 ساعة</td></tr>
+              <tr><td>بقية محافظات العراق</td><td>5,000 د.ع</td><td>24 - 24 ساعة</td></tr>
             </tbody>
           </table>
 
