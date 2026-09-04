9d5aae9a570fa8c157aea0c0be1a73cc869d685b
zeralook <za10az10plaqxm3@gmail.com>
2026-09-04 19:46:55 +0300

    Delete src/components/Toast.jsx

--- src/components/Toast.jsx
+++
@@ -1,5 +0,0 @@
-import Icon3D from "./Icon3D.jsx";
-export default function Toast({ text }) {
-  if (!text) return null;
-  return <div className="toast show"><Icon3D name="check" size={36} /> <span>{text}</span></div>;
-}
